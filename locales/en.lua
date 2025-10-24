1140d1fece2316e75cce735876cd6f11c88d9a9c
Jim Shield <gothicjim@gmail.com>
2025-10-24 15:32:48 +0100

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,47 @@
+Loc = Loc or {}
+
+Loc["en"] = {
+    error = {
+        noSpace = "Not enough space in inventory",
+        noMoney = "Not enough money",
+        incorrectAmount = "Incorrect Amount!",
+        noItems = "Error - No products found for shop",
+    },
+    basket = {
+        balance = "Balance",
+        totalCost = "Total Cost:",
+        itemsBasket = "Items in Basket:",
+        clearBasket = "Clear Basket",
+        basketHeader = "Basket",
+        removeBasket = "Remove Item",
+        editAmount = "Edit Amount",
+        editTxt = "Edit the amount of this item in your basket",
+        basketEmpty = "Basket Empty",
+        itemBasket = "Item Basket",
+        items = "Items(s)"
+    },
+    general = {
+        free = "Free",
+        confirm = "Confirm Purchase",
+        amount = "Amount",
+        amountToBuy = "Amount To Buy",
+        cost = "Cost:",
+        costPerItem = "Cost Per Item:",
+        paymentType = "Payment Type",
+        numProducts = "Products",
+        goBack = "Go Back",
+        weight = "Weight:",
+        outOfStock = "Out of Stock",
+        chargeSociety = "Charging Society for purchase",
+        freeItem = "Free Item",
+        cash = "Cash",
+        card = "Card",
+        society = "Society",
+        addedToBasket = "Added %s Item(s) to basket"
+    },
+    target = {
+        shopBlip = "Shop",
+        sellerBlip = "Shop",
+        browseShop = "Browse Shop",
+    },
+}
